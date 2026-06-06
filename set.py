cfdab33fdc0666a0b756ab17fdd64d9bff635578
gopi <gopigangula2004@gmail.com>
2026-06-06 10:48:57 +0530

    set methods

---
+++ set.py
@@ -0,0 +1,19 @@
+#set- it is also a data type which stores same set of data
+# set is created by using {}
+# set doesn't support indexing becuase of no particular sequence of the output
+# set is mutable data type and it stores unique values
+set={1,34,56,78,97,97,78}
+print(set)
+# when we print the set it removes duplicate values and prints only unique values
+# when we print the set it doesn't provide the same order of sequence as we provided in the input so that's the reason
+# set doesn't support indexing
+# set also have different built-in methods
+# add-used to add a values to set
+set.add(45)
+print(set)
+# update- used to add multipile values
+set.update([18,46,76])
+print(set)
+#remove -used to remove a particullar element
+set.remove(78)
+print(set)
