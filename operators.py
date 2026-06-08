6ec653c9e5a3b33efa249abff4b014e4a423a0f1
gopi <gopigangula2004@gmail.com>
2026-06-08 16:53:47 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,35 @@
+# operators in python
+# Operator Type       	Examples
+# Arithmetic Operators	 +, -, *, /
+# Assignment Operators	 =, +=, -=
+# Comparison Operators	 ==, !=, >
+# Logical Operators	     and, or, not
+# Bitwise Operators	     &, ^
+# Membership Operators   in, not in
+# Identity Operators	 is, is not
+# Arithmetic Operators -it is used to perform mathematical calculations
+a=45
+b=18
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
+#Assignment operators-it is used to assign a value to the variable
+x=10
+print(x)
+x+=2
+print(x)
+x-=2
+print(x)
+x/=2
+print(x)
+x//=2
+print(x)
+x%=2
+print(x)
+x=20
+x*=2
+print(x)
