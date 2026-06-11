f05b75c5638fda806e64c7598ee3813f41557ec1
gopi <gopigangula2004@gmail.com>
2026-06-11 11:46:50 +0530

    patterns on square

---
+++ square pattern.py
@@ -0,0 +1,22 @@
+# pattern's in python can be printed by using loop's either while or for by using *,alphabets or numbers
+# We can print square pattern with two loops ,one loop for rows and another loop for columns
+# we can print using by one single loop with the help of string multiplication
+# square pattern using two loops
+# for i in range(5): # oter loop for flow (rows)
+#     for j in range(5): # inner loop for printing stars(columns)
+#         print("*",end=" ")
+#     print()# for new line
+
+# square pattern in one loop using string multiplication
+# for i in range(5):
+# #     print("* "*5)
+# for i in range(5):
+#     for j in range(5):
+#          print(i,end=" ")
+#     print()
+# for i in range(5):
+#     for j in range(5):
+#          print(j,end=" ")
+#     print()
+# for i in range(5):
+#     print("1 "*5)
