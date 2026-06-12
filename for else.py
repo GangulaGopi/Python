c1870e9fee00b3544b45d07b87796abefbeb65f5
gopi <gopigangula2004@gmail.com>
2026-06-12 22:27:47 +0530

    for else loop

---
+++ for else.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3]
+
+for n in numbers:
+    print(n)
+else:
+    print("Loop finished")
+
+numbers = [1, 2, 3]
+
+for n in numbers:
+    print(n)
+    if n == 2:
+        break
+else:
+    print("Loop finished")
