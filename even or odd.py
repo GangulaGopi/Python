0b8c6f5cad055a511392a698bc96858b53af3e40
gopi <gopigangula2004@gmail.com>
2026-06-17 21:34:42 +0530

    finding even or odd

---
+++ even or odd.py
@@ -0,0 +1,20 @@
+# Checking whether the given number is even or odd
+# even number - it means the number is divisible by zero and returns zero then it is even or odd
+# n=int(input("enter a number:"))
+# if n%2==0:
+#     print("even Number")
+# else:
+#     print("odd Number")
+
+# finding even numbers within the range
+# def even(n):
+#     if n%2==0:
+#         return True
+#     else:
+#         return False
+n=int(input())
+for i in range(1,n):
+    if i%2==0:
+        print("Even",i)
+    else:
+        print("odd",i)
